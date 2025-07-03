6318fa6010739adb3e6b9ca6c9d3e8a535bf47ec
aitoAarni <aarni.rechardt@gmail.com>
2025-07-03 17:51:37 +0300

    add endpoint

--- timestamp/timestamp.py
+++ timestamp/timestamp.py
@@ -2,10 +2,21 @@ import datetime
 import string
 import random
 import time
+from fastapi import FastAPI
 
 chars = string.ascii_letters + string.digits + string.punctuation
 hash = "".join([random.choice(chars) for i in range(0, random.randint(48, 64))]).strip()
 
-while True:
-    print(f"{datetime.datetime.now()}: {hash}")
-    time.sleep(5)
+def get_stamp():
+    return f"{datetime.datetime.now()}: {hash}"
+
+if __name__ == "__main__":
+    app = FastAPI()
+
+    @app.get("/")
+    def get_samp():
+        return {"message": get_stamp()}
+
+    while True:
+        print(get_stamp())
+        time.sleep(5)
