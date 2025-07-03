a6ef047a5bdecaa93fad9b65dc1ae05276d55c50
aitoAarni <aarni.rechardt@gmail.com>
2025-07-03 19:29:24 +0300

    exercise 1.7 done

--- timestamp/timestamp.py
+++ timestamp/timestamp.py
@@ -2,6 +2,7 @@ import datetime
 import string
 import random
 import time
+import threading
 from fastapi import FastAPI
 
 chars = string.ascii_letters + string.digits + string.punctuation
@@ -10,13 +11,19 @@ hash = "".join([random.choice(chars) for i in range(0, random.randint(48, 64))])
 def get_stamp():
     return f"{datetime.datetime.now()}: {hash}"
 
-if __name__ == "__main__":
-    app = FastAPI()
+# if __name__ == "__main__":
+app = FastAPI()
 
-    @app.get("/")
-    def get_samp():
-        return {"message": get_stamp()}
+@app.get("/")
+def get_samp():
+    return {"message": get_stamp()}
 
+def print_loop():
     while True:
         print(get_stamp())
         time.sleep(5)
+
+@app.on_event("startup")
+def start_background_task():
+    thread = threading.Thread(target=print_loop, daemon=True)
+    thread.start()
