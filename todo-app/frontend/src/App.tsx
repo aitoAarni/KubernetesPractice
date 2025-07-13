e3e19333d099ad35ab13db3e8b8d7d6c2c46a625
aitoAarni <aarni.rechardt@gmail.com>
2025-07-13 12:24:30 +0200

    exercise 1.13 done

--- todo-app/frontend/src/App.tsx
+++ todo-app/frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import { imageUrl } from "./config"
 
 function App() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [todos, _] = useState<string[]>([
+        "Learn React",
+        "Learn TypeScript",
+        "Learn Kubernetes",
+    ])
     return (
         <div style={{ padding: "20px" }}>
             <h1>The project App</h1>
@@ -10,8 +17,16 @@ function App() {
                 width="500px"
                 height="700px"
             />
+            <br />
+            <input type="text" maxLength={140} />
+            <button>Create todo</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
             <p>DevOps with Kubernetes 2025</p>
-        </div >
+        </div>
     )
 }
 
