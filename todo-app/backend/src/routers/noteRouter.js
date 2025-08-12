0f6c36e8f6ade777ae7207fdbda52e347213b175
aitoAarni <aarni.rechardt@gmail.com>
2025-08-12 11:41:09 +0200

    exercise 2.10 done

--- todo-app/backend/src/routers/noteRouter.js
+++ todo-app/backend/src/routers/noteRouter.js
@@ -4,6 +4,7 @@ import { addNote, getNotes } from "../services/noteService.js"
 const noteRouter = Router()
 
 noteRouter.get("/", async (req, res, next) => {
+    console.log("Fetching all notes")
     const notes = await getNotes()
 
     res.status(200).json({ data: notes })
@@ -12,8 +13,16 @@ noteRouter.get("/", async (req, res, next) => {
 noteRouter.post("/", async (req, res, next) => {
     const note = req.body.data
     if (!note) {
+        console.error("Note content is required")
         return res.status(400).json({ error: "Note content is required" })
     }
+    if (note.length > 140) {
+        console.error("Note content exceeds 140 characters: ", note)
+        return res
+            .status(400)
+            .json({ error: "Note content exceeds 140 characters" })
+    }
+    console.log("Adding a new note:", note)
     const addedNote = await addNote(note)
     res.status(201).json({ data: addedNote })
 })
